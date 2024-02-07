1a23125adcc2323ca4dac64486ad31ba287a6cb9
Camillemtd <camille_mtd@hotmail.com>
2024-02-07 13:52:03 -0500

    create user

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,11 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcrypt";
 
 import { RegisterSchema } from "@/schemas";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
 	const validatedField = RegisterSchema.safeParse(values);
@@ -10,5 +13,25 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
 	if(!validatedField.success) {
 		return {error: "Invalid fileds"};
 	}
-	return {success: "Email sent!"};
+
+	const {email, password, name} = validatedField.data
+	const hashedPassword = await bcrypt.hash(password, 10);
+
+	const existingUser = await getUserByEmail(email);
+
+	if(existingUser) {
+		return { error: "Email already exists" };
+	}
+
+	await db.user.create({
+		data: {
+			email,
+			password: hashedPassword,
+			name,
+		}
+	});
+
+	//TODO: send veriiication email
+
+	return {success: "User created!"};
 }
