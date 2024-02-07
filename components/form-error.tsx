37c2978e21e80c3d6010174b9ee833465fe499b7
Camillemtd <camille_mtd@hotmail.com>
2024-02-07 10:05:28 -0500

    register form

--- components/form-error.tsx
+++ components/form-error.tsx
@@ -5,6 +5,8 @@ interface FormErrorProps {
 }
 
 export default function FormError({ message }: FormErrorProps) {
+	if(!message) return null;
+	
 	  return (
 		      <div className="flex items-center bg-destructive/15 p-3 rounded-md gap-x-2 text-sm text-destructive">
 		        <ExclamationTriangleIcon />
--- components/form-success.tsx
+++ components/form-success.tsx
@@ -5,6 +5,8 @@ interface FormSuccessProps {
 }
 
 export default function FormSuccess({ message }: FormSuccessProps) {
+	if(!message) return null;
+	
 	  return (
 		      <div className="flex items-center bg-emerald-500/15 p-3 rounded-md gap-x-2 text-sm text-emerald-500">
 		        <CheckCircledIcon />
